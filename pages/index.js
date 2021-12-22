0c496ac4994cbab72894eb6ea73340c71c9976cf
Celeste Rowe <celeste.rowe@perficient.com>
2021-12-22 18:20:41 -0500

    home page with dummy meetups

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'Coming Soon',
+    image: 'https://unsplash.com/photos/nUkxLPE5Fto',
+    address: 'NOLA',
+    description: 'First vacay'
+  },
+  {
+    id: 'm2',
+    title: 'Beach Life',
+    image: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.climatechangenews.com%2Ffiles%2F2020%2F07%2F02123958%2Fjamaica.jpg&imgrefurl=https%3A%2F%2Fwww.climatechangenews.com%2F2020%2F07%2F02%2Fjamaica-becomes-first-caribbean-nation-submit-tougher-climate-plan-un%2F&tbnid=mlpLMEGMVI3FaM&vet=12ahUKEwjTmdiRxfj0AhVGUs0KHdLWCXYQMygOegUIARDnAQ..i&docid=OEVeP-8JWHTQBM&w=796&h=448&q=jamaica&ved=2ahUKEwjTmdiRxfj0AhVGUs0KHdLWCXYQMygOegUIARDnAQ',
+    address: 'Jamaica',
+    description: 'Second vacay'
+  }
+];
+
+function Home () {
+  return (
+  <MeetupList meetups={DUMMY_MEETUPS} />
+  )}
+
+export default Home;
---
+++ pages/new-meetup/index.js
@@ -0,0 +1,7 @@
+// our-domain.com/new-meetup
+
+function Home () {
+  return <h1>Hey</h1>
+}
+
+export default Home;
