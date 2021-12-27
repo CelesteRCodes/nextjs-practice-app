4ddafd9f1e5598e376cf5b3db55524898d8ee621
Celeste Rowe <celeste.rowe@perficient.com>
2021-12-27 11:22:05 -0500

    basic API route

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,28 @@
+import { MongoClient } from "mongodb";
 // url = /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    // const { title, image, address, description } = data;
 
+    const client = await MongoClient.connect(
+      "mongodb+srv://celeste:marie313@cluster0.tnhom.mongodb.net/meetupsAPIpractice?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne({ data });
+
+    console.log(result);
+
+    client.close(); 
+    // closes db 
+
+    res.status(201).json({message: 'Meetup inserted'});
   }
 }
 
